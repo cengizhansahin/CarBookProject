ad9dc1397bff25f3f5637955902ad356f3a1201f
Cengizhan <135475831+cengizhansahin@users.noreply.github.com>
2024-06-26 14:17:46 +0300

    CarPricing, Contact ve Pricing sınıfları eklendi. Car sınıfı yeni eklenen sınıflara göre güncellendi.

--- Core/CarBookProject.Domain/Entities/Car.cs
+++ Core/CarBookProject.Domain/Entities/Car.cs
@@ -21,5 +21,7 @@ namespace CarBookProject.Domain.Entities
         public string BigImageUrl { get; set; }
         public List<CarFeature> CarFeatures { get; set; }
         public List<CarDescription> CarDescriptions { get; set; }
+        public List<CarPricing> CarPricings { get; set; }
+
     }
 }
