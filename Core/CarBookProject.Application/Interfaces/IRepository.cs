293fb1fa051460a013792ce5186f4e52da9b8d50
Cengizhan <135475831+cengizhansahin@users.noreply.github.com>
2024-06-26 16:41:42 +0300

    IRepository içerisi dolduruldu

--- Core/CarBookProject.Application/Interfaces/IRepository.cs
+++ Core/CarBookProject.Application/Interfaces/IRepository.cs
@@ -9,9 +9,9 @@ namespace CarBookProject.Application.Interfaces
     public interface IRepository<T> where T : class
     {
         Task<List<T>> GetAllAsync();
-        Task<T> GetAsync(int id);
+        Task<T> GetByIdAsync(int id);
         Task CreateAsync(T entity);
         Task UpdateAsync(T entity);
-        Task RemoveAsync(int id);
+        Task RemoveAsync(T entity);
     }
 }
