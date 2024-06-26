6fee31804bf30c1574021b4472a78ec636af2303
Cengizhan <135475831+cengizhansahin@users.noreply.github.com>
2024-06-26 17:08:57 +0300

    GetAboutQueryHandler yazıldı

---
+++ Core/CarBookProject.Application/Features/CQRS/Handlers/AboutHandlers/GetAboutQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using CarBookProject.Application.Features.CQRS.Results.AboutResult;
+using CarBookProject.Application.Interfaces;
+using CarBookProject.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBookProject.Application.Features.CQRS.Handlers.AboutHandlers
+{
+    public class GetAboutQueryHandler
+    {
+        private readonly IRepository<About> _repository;
+        public GetAboutQueryHandler(IRepository<About> repository)
+        {
+            _repository = repository;
+        }
+        public async Task<List<GetAboutQueryResult>> Handle()
+        {
+            var values = await _repository.GetAllAsync();
+            return values.Select(x => new GetAboutQueryResult
+            {
+                AboutID = x.AboutID,
+                Description = x.Description,
+                ImageUrl = x.ImageUrl,
+                Title = x.Title,
+            }).ToList();
+        }
+    }
+}
