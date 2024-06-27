2b1fdd7bf3d61c28fd654a984f22b12b433597ac
Cengizhan <135475831+cengizhansahin@users.noreply.github.com>
2024-06-28 00:14:28 +0300

    UpdateAboutCommandHandler yazıldı

---
+++ Core/CarBookProject.Application/Features/CQRS/Handlers/AboutHandlers/UpdateAboutCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using CarBookProject.Application.Features.CQRS.Commands.AboutCommands;
+using CarBookProject.Application.Interfaces;
+using CarBookProject.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBookProject.Application.Features.CQRS.Handlers.AboutHandlers
+{
+    public class UpdateAboutCommandHandler
+    {
+        private readonly IRepository<About> _repository;
+
+        public UpdateAboutCommandHandler(IRepository<About> repository)
+        {
+            _repository = repository;
+        }
+        public async Task Handle(UpdateAboutCommand command)
+        {
+            var values = await _repository.GetByIdAsync(command.AboutID);
+            values.Description = command.Description;
+            values.ImageUrl = command.ImageUrl;
+            values.Title = command.Title;
+            await _repository.UpdateAsync(values);
+        }
+    }
+}
