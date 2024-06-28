9e324630ebdcd7a72ca3c89c2bb15609177cce64
Cengizhan <135475831+cengizhansahin@users.noreply.github.com>
2024-06-29 00:33:43 +0300

    Banner Api Test Edildi

---
+++ Presentation/CarBookProject.WebApi/Controllers/Banners1Controller.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CarBookProject.Domain.Entities;
+using CarBookProject.Persistence.Context;
+
+namespace CarBookProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Banners1Controller : ControllerBase
+    {
+        private readonly CarBookContext _context;
+
+        public Banners1Controller(CarBookContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Banners1
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Banner>>> GetBanners()
+        {
+            return await _context.Banners.ToListAsync();
+        }
+
+        // GET: api/Banners1/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Banner>> GetBanner(int id)
+        {
+            var banner = await _context.Banners.FindAsync(id);
+
+            if (banner == null)
+            {
+                return NotFound();
+            }
+
+            return banner;
+        }
+
+        // PUT: api/Banners1/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBanner(int id, Banner banner)
+        {
+            if (id != banner.BannerID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(banner).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BannerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Banners1
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Banner>> PostBanner(Banner banner)
+        {
+            _context.Banners.Add(banner);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBanner", new { id = banner.BannerID }, banner);
+        }
+
+        // DELETE: api/Banners1/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBanner(int id)
+        {
+            var banner = await _context.Banners.FindAsync(id);
+            if (banner == null)
+            {
+                return NotFound();
+            }
+
+            _context.Banners.Remove(banner);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BannerExists(int id)
+        {
+            return _context.Banners.Any(e => e.BannerID == id);
+        }
+    }
+}
--- Presentation/CarBookProject.WebApi/Program.cs
+++ Presentation/CarBookProject.WebApi/Program.cs
@@ -1,4 +1,5 @@
 using CarBookProject.Application.Features.CQRS.Handlers.AboutHandlers;
+using CarBookProject.Application.Features.CQRS.Handlers.BannerHandlers;
 using CarBookProject.Application.Interfaces;
 using CarBookProject.Persistence.Context;
 using CarBookProject.Persistence.Repositoies;
@@ -16,6 +17,12 @@ builder.Services.AddScoped<GetAboutQueryHandler>();
 builder.Services.AddScoped<CreateAboutCommandHandler>();
 builder.Services.AddScoped<RemoveAboutCommandHandler>();
 builder.Services.AddScoped<UpdateAboutCommandHandler>();
+
+builder.Services.AddScoped<GetBannerByIdQueryHandler>();
+builder.Services.AddScoped<GetBannerQueryHandler>();
+builder.Services.AddScoped<CreateBannerCommandHandler>();
+builder.Services.AddScoped<RemoveBannerCommandHandler>();
+builder.Services.AddScoped<UpdateBannerCommandHandler>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
