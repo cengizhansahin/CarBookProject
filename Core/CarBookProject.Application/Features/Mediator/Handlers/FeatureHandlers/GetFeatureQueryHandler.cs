2ada06f7ada051817566893868920880e65e380c
Cengizhan <135475831+cengizhansahin@users.noreply.github.com>
2024-07-04 01:26:08 +0300

    Featured command handler yazıldı

--- Core/CarBookProject.Application/Features/Mediator/Handlers/FeatureHandlers/GetFeatureQueryHandler.cs
+++ Core/CarBookProject.Application/Features/Mediator/Handlers/FeatureHandlers/GetFeatureQueryHandler.cs
@@ -13,12 +13,12 @@ namespace CarBookProject.Application.Features.Mediator.Handlers.FeatureHandlers
 {
     public class GetFeatureQueryHandler : IRequestHandler<GetFeatureQuery, List<GetFeatureQueryResult>>
     {
-        private readonly IRepository<Feature> _repository;
+            private readonly IRepository<Feature> _repository;
 
-        public GetFeatureQueryHandler(IRepository<Feature> repository)
-        {
-            _repository = repository;
-        }
+            public GetFeatureQueryHandler(IRepository<Feature> repository)
+            {
+                _repository = repository;
+            }
 
         public async Task<List<GetFeatureQueryResult>> Handle(GetFeatureQuery request, CancellationToken cancellationToken)
         {
