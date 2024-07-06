4616bc8ff3ca1ca06be4a7e5d44c7e55f027ef1c
Cengizhan <135475831+cengizhansahin@users.noreply.github.com>
2024-07-07 02:20:27 +0300

    About consume kodları

--- FrontEnds/CarBookProject.WebUI/ViewComponents/AboutViewComponents/_AboutUsComponentPartial.cs
+++ FrontEnds/CarBookProject.WebUI/ViewComponents/AboutViewComponents/_AboutUsComponentPartial.cs
@@ -1,11 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBookProject.WebUI.ViewComponents.AboutViewComponents
 {
     public class _AboutUsComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _AboutUsComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7157/api/Abouts");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<>>
+            }
             return View();
         }
     }
