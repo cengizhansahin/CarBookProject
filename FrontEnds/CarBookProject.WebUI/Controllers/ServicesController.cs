7ef38744ebb6593d0bcd3bccb3108280d5b6dccf
Cengizhan <135475831+cengizhansahin@users.noreply.github.com>
2024-07-08 01:42:29 +0300

    hizmetler bölümü listelendi

---
+++ FrontEnds/CarBookProject.WebUI/Controllers/ServicesController.cs
@@ -0,0 +1,29 @@
+﻿using CarBookProject.Dto.ServiceDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBookProject.WebUI.Controllers
+{
+    public class ServicesController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ServicesController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7157/api/Services");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
