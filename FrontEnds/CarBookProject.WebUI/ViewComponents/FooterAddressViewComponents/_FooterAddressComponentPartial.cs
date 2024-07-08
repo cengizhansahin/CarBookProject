3df3118b04e74034da8d58083f81a212063bfdc8
Cengizhan <135475831+cengizhansahin@users.noreply.github.com>
2024-07-08 20:11:40 +0300

    Adres bilgileri listelendi

---
+++ FrontEnds/CarBookProject.WebUI/ViewComponents/FooterAddressViewComponents/_FooterAddressComponentPartial.cs
@@ -0,0 +1,32 @@
+﻿using CarBookProject.Dto.FooterAddressDtos;
+using CarBookProject.Dto.TestimonialDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBookProject.WebUI.ViewComponents.FooterAddressViewComponents
+{
+    public class _FooterAddressComponentPartial : ViewComponent
+    {
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _FooterAddressComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7157/api/FooterAddresses");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFooterAddressDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
+
