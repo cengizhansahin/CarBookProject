4b8cf1bf22e5c8c4924b11bcf6b64f4210b1f368
Cengizhan <135475831+cengizhansahin@users.noreply.github.com>
2024-10-17 00:04:33 +0300

    Hizmetlerin ana sayfaya eklenme işlemi yapıldı

---
+++ FrontEnds/CarBookProject.WebUI/ViewComponents/ServiceViewComponents/_ServiceViewComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using CarBookProject.Dto.FooterAddressDtos;
+using CarBookProject.Dto.ServiceDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBookProject.WebUI.ViewComponents.ServiceViewComponents
+{
+    public class _ServiceViewComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _ServiceViewComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7157/api/Services");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
