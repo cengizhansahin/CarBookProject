2ff5d07512f23acf5e3bcd24d8d7d70dac1238e0
Cengizhan <135475831+cengizhansahin@users.noreply.github.com>
2024-10-20 03:04:42 +0300

    son 3 bloğun yazarıyla getirilmesi işlemi uygulandı

---
+++ Core/CarBookProject.Application/Features/Mediator/Handlers/BlogHandlers/GetLast3BlogsWithAuthorsQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using CarBookProject.Application.Features.Mediator.Queries.BlogQueries;
+using CarBookProject.Application.Features.Mediator.Results.BlogResults;
+using CarBookProject.Application.Interfaces.BlogInterfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBookProject.Application.Features.Mediator.Handlers.BlogHandlers
+{
+    public class GetLast3BlogsWithAuthorsQueryHandler : IRequestHandler<GetLast3BlogsWithAuthorsQuery, List<GetLast3BlogsWithAuthorsQueryResult>>
+    {
+        private readonly IBlogRepository _blogRepository;
+
+        public GetLast3BlogsWithAuthorsQueryHandler(IBlogRepository blogRepository)
+        {
+            _blogRepository = blogRepository;
+        }
+
+        public async Task<List<GetLast3BlogsWithAuthorsQueryResult>> Handle(GetLast3BlogsWithAuthorsQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _blogRepository.GetLast3BlogsWithAuthors();
+            var newValues = values.Select(x => new GetLast3BlogsWithAuthorsQueryResult
+            {
+                BlogId = x.BlogId,
+                AuthorId = x.AuthorId,
+                BlogTitle = x.BlogTitle,
+                CategoryId = x.CategoryId,
+                CoverImageUrl = x.CoverImageUrl,
+                CreatedDate = x.CreatedDate,
+                AuthorName = x.Author.AuthorName
+            }).ToList();
+            return newValues;
+        }
+    }
+}
---
+++ Infrastructure/CarBookProject.Persistence/Repositories/BlogRepositories/BlogRepository.cs
@@ -0,0 +1,28 @@
+﻿using CarBookProject.Application.Interfaces.BlogInterfaces;
+using CarBookProject.Domain.Entities;
+using CarBookProject.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBookProject.Persistence.Repositories.BlogRepositories
+{
+    public class BlogRepository : IBlogRepository
+    {
+        private readonly CarBookContext _context;
+
+        public BlogRepository(CarBookContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Blog>> GetLast3BlogsWithAuthors()
+        {
+            var values = await _context.Blogs.Include(x => x.Author).OrderByDescending(x => x.BlogId).Take(3).ToListAsync();
+            return values;
+        }
+    }
+}
