3217e8824b8bcbca2aa8cb1d8bc326607e32e00b
Cengizhan <135475831+cengizhansahin@users.noreply.github.com>
2024-10-28 02:27:45 +0300

    blogların frontend de listelenmesi

---
+++ FrontEnds/CarBookProject.Dto/BlogDtos/ResultAllBlogsWithAuthorsDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBookProject.Dto.BlogDtos
+{
+    public class ResultAllBlogsWithAuthorsDto
+    {
+
+        public int blogId { get; set; }
+        public string blogTitle { get; set; }
+        public string authorName { get; set; }
+        public string categoryName { get; set; }
+        public int authorId { get; set; }
+        public string coverImageUrl { get; set; }
+        public DateTime createdDate { get; set; }
+        public int categoryId { get; set; }
+
+    }
+}
---
+++ FrontEnds/CarBookProject.WebUI/Controllers/BlogController.cs
@@ -0,0 +1,31 @@
+﻿using CarBookProject.Dto.BlogDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBookProject.WebUI.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BlogController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.v1 = "Bloglar";
+            ViewBag.v2 = "Yazarlarımızın Blogları";
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7157/api/Blogs/GetAllBlogsWithAuthors");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAllBlogsWithAuthorsDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
