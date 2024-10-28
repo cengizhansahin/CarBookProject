637bc06b0d04d2a7e9e2c01eeec6ca436385e2f5
Cengizhan <135475831+cengizhansahin@users.noreply.github.com>
2024-10-28 04:09:07 +0300

    blog detayları viewcomponentler

---
+++ FrontEnds/CarBookProject.WebUI/ViewComponents/BlogViewComponents/_BlogDetailMainComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBookProject.WebUI.ViewComponents.BlogViewComponents
+{
+    public class _BlogDetailMainComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() { return View(); }
+    }
+}
---
+++ FrontEnds/CarBookProject.WebUI/ViewComponents/BlogViewComponents/_BlogDetailTagCloudComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBookProject.WebUI.ViewComponents.BlogViewComponents
+{
+    public class _BlogDetailTagCloudComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() { return View(); }
+    }
+}
