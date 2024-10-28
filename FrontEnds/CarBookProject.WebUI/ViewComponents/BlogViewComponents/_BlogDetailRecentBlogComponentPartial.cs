61f62b1998287bf0bed88f3403e1599eb7fa8e9d
Cengizhan <135475831+cengizhansahin@users.noreply.github.com>
2024-10-28 14:50:36 +0300

    blog detaylarında son blog listesi

--- FrontEnds/CarBookProject.WebUI/ViewComponents/BlogViewComponents/_BlogDetailRecentBlogComponentPartial.cs
+++ FrontEnds/CarBookProject.WebUI/ViewComponents/BlogViewComponents/_BlogDetailRecentBlogComponentPartial.cs
@@ -1,9 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBookProject.Dto.BlogDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBookProject.WebUI.ViewComponents.BlogViewComponents
 {
     public class _BlogDetailRecentBlogComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke() { return View(); }
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _BlogDetailRecentBlogComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7157/api/Blogs/GetLast3BlogsWithAuthors");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultLast3BlogsWithAuthorsDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
     }
 }
