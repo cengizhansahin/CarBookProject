47c17069c7e1a744d2fc68c22612466d9aa1987a
Cengizhan <135475831+cengizhansahin@users.noreply.github.com>
2024-10-28 14:45:23 +0300

    blog detaylarında kategori listesi

--- FrontEnds/CarBookProject.WebUI/ViewComponents/BlogViewComponents/_BlogDetailCategoryComponentPartial.cs
+++ FrontEnds/CarBookProject.WebUI/ViewComponents/BlogViewComponents/_BlogDetailCategoryComponentPartial.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBookProject.Dto.CategoryDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBookProject.WebUI.ViewComponents.BlogViewComponents
 {
     public class _BlogDetailCategoryComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _BlogDetailCategoryComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7157/api/Categories");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
