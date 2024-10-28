63d43f62a68d6c84458f910c474cd35b611d7b7a
Cengizhan <135475831+cengizhansahin@users.noreply.github.com>
2024-10-28 04:29:22 +0300

    yorum listesi - yeni yorum - etiket bulutu sayfaları view component olarak düzenlendi

---
+++ FrontEnds/CarBookProject.WebUI/ViewComponents/BlogViewComponents/_BlogDetailAuthorAboutComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBookProject.WebUI.ViewComponents.BlogViewComponents
+{
+    public class _BlogDetailAuthorAboutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() { return View(); }
+    }
+}
---
+++ FrontEnds/CarBookProject.WebUI/ViewComponents/CommentViewComponents/_AddCommentComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBookProject.WebUI.ViewComponents.CommentViewComponents
+{
+    public class _AddCommentComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() { return View(); }
+    }
+}
